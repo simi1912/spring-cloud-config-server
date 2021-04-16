f39c4d8ac0b4c6a9315c1a64808818d294ed8cc6
Simi <nioasca@gmail.com>
2021-04-16 13:32:08 +0300

    qa min 222

---
+++ src/test/java/com/simiv1/microservices/springcloudconfigserver/SpringCloudConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.simiv1.microservices.springcloudconfigserver;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class SpringCloudConfigServerApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
